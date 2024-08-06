974c49f07f3df35b534978c563cac177a8abcd76
Tioman Gally <tioman.gally@novatec-gmbh.de>
2024-08-06 13:51:11 +0200

    Bonsai: Initial Commit

---
+++ src/main/kotlin/de/gally/bonsai/BonsaiApplication.kt
@@ -0,0 +1,11 @@
+package de.gally.bonsai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BonsaiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BonsaiApplication>(*args)
+}
